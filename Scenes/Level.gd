be7c8807a2e3a6e26316bd2cabd29e96d176849b
Eric Johne <ericjohne2001@gmail.com>
2022-05-17 14:44:46 +0200

    Helper class Graph added to Level class, Helper class SetMethods added to project.

--- Scenes/Level.gd
+++ Scenes/Level.gd
@@ -1,13 +1,30 @@
 extends Node2D
 
+##
+## This is the Level class of the game 'Escape the Graph'
+##
+## @desc:
+##		The Level class will provide the functionality to generate Levels,
+##		the possibility to interact with them and the system to evaluate the interaction.
+##
 
+# Parameters
 export (PackedScene) var node_scene
 export (PackedScene) var edge_scene
+## @desc: 
+## 		grid size for distribution of nodes -> 2 -> 4 grid fields for distribution
 export var grid : int = 2
-export var margin = 200.0
-export var node_number : int = 1
-export var edges : int = 1
+export var margin = 200.0 # border margin in the scene
+export var node_number : int = 1 # number of nodes in the graph
+export var edges : int = 1 # number of edges in the graph
+enum ProblemType {SHORTEST_PATH, CAPACITY_PROBLEM, RELIABILITY_PROBLEM}
+export (ProblemType) var type 
+export var minimal_weight = 0.0 # minimal weight of edges
+export var maximum_weight = 10.0 # maximum weight of edges
+
+# Attributes
 var screen_size
+const SetMethods = preload("SetMethods.gd")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -78,7 +95,6 @@ func _on_Generate_pressed():
 	get_tree().call_group("Graph", "queue_free")
 	distribute_objects(screen_size, grid, margin, node_number, edges)
 
-# DEBUGGING NEEDED!!!
 func edge_exists_already(edge, edges) -> bool:
 	if edge == null or edges == []:
 		return false 
@@ -86,3 +102,57 @@ func edge_exists_already(edge, edges) -> bool:
 		if edge.hash() == edges[i].hash():
 			return true
 	return false
+
+class Graph:
+	var nodes = [] # contains all nodes
+	var edges = [] # contains all edges
+	var start = null
+	var target = null
+	
+	func _init():
+		pass
+	
+	func add_node(node):
+		if (node in self.nodes) == false: nodes.append(node)
+		return nodes
+	
+	func add_edge(node1, node2, weight):
+		if node1 in self.nodes and node2 in self.nodes:
+			var edge = {
+				'targeting': {node1: node2, node2: node1},
+				'weight': weight
+			}
+			self.edges.append(edge)
+	
+	func get_neighbours(node):
+		var neighbours = []
+		for edge in self.edges:
+			for start in edge['targeting'].keys():
+				if start == node:
+					neighbours.append(edge['targeting'][node])
+		return neighbours
+	
+	func get_edge_weight(node1, node2):
+		for edge in self.edges:
+			for start in edge['targeting'].keys():
+				if start == node1 and edge['targeting'][start] == node2:
+					return edge['weight']
+	
+	func define_start():
+		if self.nodes !=  []:
+			self.start = nodes[randi() % self.nodes.size()]
+	
+	func define_target(steps):
+		var excluded_nodes = [self.start]
+		for i in range(steps):
+			for e in excluded_nodes:
+				excluded_nodes = SetMethods.union(excluded_nodes, self.get_neighbours(e))
+		
+		var possible_nodes = SetMethods.difference(self.nodes, excluded_nodes)
+		if possible_nodes == []:
+			possible_nodes = SetMethods.difference(self.nodes, [self.start])
+		if possible_nodes == []:
+			return
+		
+		self.target = possible_nodes[randi() % possible_nodes.size()]
+		return self.target
---
+++ Scenes/SetMethods.gd
@@ -0,0 +1,25 @@
+extends Node
+
+static func intersect(array1 = [], array2 = []):
+	var intersection = []
+	for e in range(array1.size()):
+		if array2.has(array1[e]):
+			intersection.append(array1[e])
+	return intersection
+
+static func union(array1 = [], array2 = []):
+	var union_arr = []
+	
+	for e in range(array1.size()):
+		if (union_arr.has(array1[e])) == false: union_arr.append(array1[e])
+	for e in range(array2.size()):
+		if (union_arr.has(array2[e])) == false: union_arr.append(array2[e])
+	return union_arr
+	
+static func difference(array1 = [], array2 = []):
+	var diff = [] + array1
+	
+	for e in range(array1.size()):
+		if array2.has(array1[e]): diff.erase(array1[e])
+	return diff
+	
