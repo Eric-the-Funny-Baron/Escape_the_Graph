c0004b24de761c8aed772d23e0f7b4363788fd24
friedaxvictoria <frieda.hentschel@gmail.com>
2022-06-01 14:10:08 +0200

    Settings kind of working

--- Scripts/Settings_Menu.gd
+++ Scripts/Settings_Menu.gd
@@ -1,8 +1,37 @@
 extends Popup
 
 
-func _ready():
-	$GridContainer/HSlider.release_focus()
-
 func _on_HSlider_mouse_exited():
 	self.release_focus()
+
+
+func _on_BrightnessSlider_value_changed(value):
+	GlobalSettings.update_brightness(value)
+
+
+func _on_MasterSlider_value_changed(value):
+	if (value < 0):
+		value = 72.0/50.0*value
+	if (value > 0):
+		value = 6.0/50.0*value
+	GlobalSettings.update_master_volume(value)
+
+
+func _on_MusicSlider_value_changed(value):
+	if (value < 0):
+		value = 72.0/50.0*value
+	if (value > 0):
+		value = 6.0/50.0*value
+	GlobalSettings.update_music_volume(value)
+
+
+func _on_EffectSlider_value_changed(value):
+	if (value < 0):
+		value = 72.0/50.0*value
+	if (value > 0):
+		value = 6.0/50.0*value
+	GlobalSettings.update_sfx_volume(value)
+
+
+func _on_ContrastSlider_value_changed(value):
+	GlobalSettings.update_contrast(value)
