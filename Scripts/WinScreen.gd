bc3dbefeb82a974c64cdea07dd774cb93542d4af
karamjanabi <karamjanabi@gmail.com>
2022-06-26 12:35:03 +0200

    the winner screen is now done with button

--- Scripts/WinScreen.gd
+++ Scripts/WinScreen.gd
@@ -4,3 +4,9 @@ func _ready():
 	Signals.emit_signal("sound_start_requested", "Win_Jingle")
 	yield(get_tree().create_timer(20), "timeout")
 	Signals.emit_signal("title_screen_requested", true)
+
+
+
+func _on_Button_pressed():
+	Signals.emit_signal("title_screen_requested", true)
+
