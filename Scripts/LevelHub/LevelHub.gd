cf4c9f0693ec08a7e816ce288b8dd200be105466
friedaxvictoria <frieda.hentschel@gmail.com>
2022-07-06 07:13:42 +0200

    final buttons

--- Scripts/LevelHub/LevelHub.gd
+++ Scripts/LevelHub/LevelHub.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+var time = 0
+var timeDirection = 1
+var moveDuration = 0.5
+
 func _ready():
 	var room_complete = get_node("..").level_log.size() > 0
+	$Arrow.hide()
 	
 	for l in get_tree().get_nodes_in_group("LevelLink"):
 		if l.level_name in get_node("..").level_log: continue
@@ -12,5 +17,18 @@ func _ready():
 	
 	if room_complete:
 		$Next_Room.set_as_opened()
-	
+		$Arrow.show()
+		
 	$Previous_Room.set_as_opened()
+	
+func _process(delta):
+
+	if (time > moveDuration or time < 0):
+		timeDirection *= -1
+
+	time += delta * timeDirection
+	var t = time / moveDuration
+
+	$Arrow.position = lerp(Vector2(384.0,349.0),Vector2(395.0,349.0), t)
+	
+
